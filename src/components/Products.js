b51408a85ab878c0d67b6bee891ebbd4e4ca2c13
Daniel Markow <daniel.markow@outlook.com>
2022-10-06 21:09:47 +0200

    cart cointent now has country, checkout country and price

--- src/components/Products.js
+++ src/components/Products.js
@@ -40,15 +40,17 @@ function Products() {
     // calculate new instock value
     const stockEntry = stock.filter((s) => s.id === itemId)[0];
     const itemName = stockEntry.name;
+    const itemPrice = stockEntry.cost;
+    const itemCountry = stockEntry.country;
     const stockIndex = stock.findIndex((obj) => obj.id === stockEntry.id);
     const newStockCount = stockEntry.instock - itemCount;
 
     if (cartContent.includes(itemName)) {
       // if the item is already in the cart
       if (itemCount <= stockEntry.instock) {
-        const newVal = cart[itemName] + 1;
+        const newVal = cart[itemName].count + 1;
 
-        setCart({...cart, [itemName]: newVal});
+        setCart({...cart, [itemName]: {...cart[itemName], count: newVal}});
 
         stock[stockIndex] = {...stockEntry, instock: newStockCount};
         setStock(stock);
@@ -56,11 +58,23 @@ function Products() {
 
     } else {
       // if the item is not in the cart
-      setCart({...cart, [itemName]: 1});
+      setCart({...cart, [itemName]: {count: 1, cost: itemPrice, country: itemCountry}});
 
       stock[stockIndex] = {...stockEntry, instock: newStockCount};
       setStock(stock);
     };
+  };
+
+  const calcCheckoutPrice = (cart) => {
+    let price = 0;
+    const key = Object.keys(cart);
+
+    key.forEach((k) => {
+      const itemPrice = cart[k].count * cart[k].cost;
+      price += itemPrice;
+    })
+
+    return price;
   };
 
   return (
@@ -73,15 +87,16 @@ function Products() {
                   <Card
                     className="products-card"
                     key={i}
+                    style={{maxWidth: "15vw"}}
                   >
-                    <Card.Img variant="top" src="https://via.placeholder.com/150x100" />
-                    <Card.Header>{s.name}</Card.Header>
+                    <Card.Header>{s.name}
+                    </Card.Header>
                     <Card.Body>
                       <form
                           onSubmit={addToCart}
                           id={s.id}
                       >
-                        Stock: {s.instock}<br />
+                        Stock: {s.instock} | Price: {s.cost}<br />
                         <Button type="submit">+</Button>
                       </form>
                     </Card.Body>
@@ -92,11 +107,17 @@ function Products() {
           <Col>
             <h3>Cart Content</h3>
             {Object.keys(cart).map((key, i) => (
-                <p key={i}>{key}: {cart[key]}</p>
+                <p key={i}>{key}: {cart[key].count}, Country: {cart[key].country}</p>
             ))}
           </Col>
           <Col>
             <h3>Check Out</h3>
+            {Object.keys(cart).map((key, i) => (
+                <p key={i}>{key}: {cart[key].count}, sum price: {cart[key].count * cart[key].cost}</p>
+            ))}<br />
+            <hr />
+            <p>Overall price: {calcCheckoutPrice(cart)}</p>
+            <Button>Check out</Button>
           </Col>
         </Row>
       </Container>
